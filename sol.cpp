bc37a88b77541bc93b8f9eee7f267f6dee44e2a7
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-21 14:41:56 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -2,50 +2,41 @@
 
 using namespace std;
 
-void Solve() {
-	// input
-	int n;
-	cin >> n;
-	vector<int> b(n);
-	for (int i = 0; i < n; i++) {
-		cin >> b[i];
-	}
-
-	// fill vector 'a' with values from 1, ..., n
-	vector<int> a(n);
-	iota(a.begin(), a.end(), 1);
-
-	// brute-force approach
-	for (int i = 0; i < n; i++) {
-		if (a[i] == b[i]) {
-			continue;
-		}
-		// check if 'a[i]' can be equal to 'b[i]' by finding
-		// the GCD of 'a[i]' and a number in the sequence
-		bool checker = false;
-		for (int j = 1; j <= n; j++) {
-			if (__gcd(a[i], j) == b[i]) {
-				checker = true;
-				break;
-			}
-		}
-		if (checker) {
-			// if yes, then set the value of 'a[i]' to 'b[i]'
-			a[i] = b[i];
-		}
-	}
-
-	// output the answer
-	cout << (a == b ? "YES" : "NO") << '\n';
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int t = 1;
-	cin >> t;
-	while (t--) {
-		Solve();
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> b(n);
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+		vector<int> a(n);
+		// fill vector 'a' with values from 1 to 'n'
+		iota(a.begin(), a.end(), 1);
+		for (int i = 0; i < n; i++) {
+			if (a[i] == b[i]) {
+				// if 'a[i]' is already equal to 'b[i]', then skip
+				continue;
+			}
+			bool checker = false;
+			for (int j = 1; j <= n; j++) {
+				// check if any GCD of 'a[i]' and 'j' is equal to 'b[i]'
+				if (__gcd(a[i], j) == b[i]) {
+					// if yes, then set 'checker' to true and end the loop
+					checker = true;
+					break;
+				}
+			}
+			if (checker) {
+				// if 'checker' is true, then set 'a[i]' to 'b[i]'
+				a[i] = b[i];
+			}
+		}
+		cout << (a == b ? "YES" : "NO") << '\n';
 	}
 	return 0;
 }
