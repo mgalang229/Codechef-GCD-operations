bd517a9327ab604a847e8aa743bef5db25b07d5d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-21 17:41:30 +0800

    Update author_sol.cpp

--- author_sol.cpp
+++ author_sol.cpp
@@ -2,32 +2,27 @@
 
 using namespace std;
 
-void Solve() {
-	int n;
-	cin >> n;
-	vector<int> b(n);
-	for (int i = 0; i < n; i++) {
-		cin >> b[i];
-	}
-	// to make vector 'a' equal to 'b', the factors of 
-	// every 'a[i]' must be present in vector 'b'
-	bool checker = true;
-	for (int i = n; i >= 1; i--) {
-		if (i % b[i - 1] != 0) {
-			checker = false;
-			break;
-		}
-	}
-	cout << (checker ? "YES" : "NO") << '\n';
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int t = 1;
-	cin >> t;
-	while (t--) {
-		Solve();
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> b(n);
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+		// check if the corresponding 'i' + 1 index is divisible by the every 'b[i]'
+		bool checker = true;
+		for (int i = n; i >= 1; i--) {
+			if (i % b[i - 1] != 0) {
+				checker = false;
+				break;
+			}
+		}
+		cout << (checker ? "YES" : "NO") << '\n';
 	}
 	return 0;
 }
