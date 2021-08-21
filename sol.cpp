f4ceb6a981d9e8f2114852eca0cccb306dd82473
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-21 16:24:14 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -2,41 +2,44 @@
 
 using namespace std;
 
+void Solve() {
+	int n;
+	cin >> n;
+	vector<int> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+	vector<int> a(n);
+	// fill all vector 'a' with values from 1 to 'n'
+	iota(a.begin(), a.end(), 1);
+	for (int i = 0; i < n; i++) {
+		if (a[i] == b[i]) {
+			continue;
+		}
+		// check if the GCD of 'a[i]' and a number from
+		// 1 to 'n' is equal to 'b[i]'
+		bool checker = false;
+		for (int j = 1; j <= n; j++) {
+			if (__gcd(a[i], j) == b[i]) {
+				checker = true;
+				break;
+			}
+		}
+		// if there is, then set the value of 'a[i]' to 'b[i]'
+		if (checker) {
+			a[i] = b[i];
+		}
+	}
+	cout << (a == b ? "YES" : "NO") << '\n';
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int tt;
-	cin >> tt;
-	while (tt--) {
-		int n;
-		cin >> n;
-		vector<int> b(n);
-		for (int i = 0; i < n; i++) {
-			cin >> b[i];
-		}
-		vector<int> a(n);
-		// fill vector 'a' with values from 1 to 'n'
-		iota(a.begin(), a.end(), 1);
-		for (int i = 0; i < n; i++) {
-			if (a[i] == b[i]) {
-				// if 'a[i]' is already equal to 'b[i]', then skip
-				continue;
-			}
-			bool checker = false;
-			for (int j = 1; j <= n; j++) {
-				// check if any GCD of 'a[i]' and 'j' is equal to 'b[i]'
-				if (__gcd(a[i], j) == b[i]) {
-					// if yes, then set 'checker' to true and end the loop
-					checker = true;
-					break;
-				}
-			}
-			if (checker) {
-				// if 'checker' is true, then set 'a[i]' to 'b[i]'
-				a[i] = b[i];
-			}
-		}
-		cout << (a == b ? "YES" : "NO") << '\n';
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		Solve();
 	}
 	return 0;
 }
